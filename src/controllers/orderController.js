d30f962369e575af8bb66993ad43eaa1de631e7a
Aditya Raj <adityaraj34@icloud.com>
2026-04-19 00:24:13 +0530

    ordedsr

---
+++ src/controllers/orderController.js
@@ -0,0 +1,25 @@
+import Order from "../models/Order.js";
+
+// CREATE ORDER
+export const createOrder = async (req, res) => {
+  const { orderItems, shippingAddress, totalPrice } = req.body;
+
+  if (!orderItems || orderItems.length === 0) {
+    return res.status(400).json({ message: "No order items" });
+  }
+
+  const order = await Order.create({
+    user: req.user._id,
+    orderItems,
+    shippingAddress,
+    totalPrice,
+  });
+
+  res.status(201).json(order);
+};
+
+// GET USER ORDERS
+export const getMyOrders = async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+};
